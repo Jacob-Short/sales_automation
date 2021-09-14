ea67313cb0f613633c694e6471fac8083701d383
Jacob-Short <thatmanjshort@gmail.com>
2021-09-13 17:56:07 -0700

    creating API class

--- sales/views.py
+++ sales/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-
+from django.views.generic import View
 
 
 @login_required(login_url='/login/')
 def homepage_view(request):
     context = {}
     return render(request, 'homepage.html', context)
+
+
+class ApiView(View):
+    '''connects to api to retrive food items'''
+    
+    def get(self, request):
+        pass
+
+
+    def post(self, request):
+        pass
